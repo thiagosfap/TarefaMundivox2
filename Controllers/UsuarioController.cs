12eb877742e8f09fa75ab627e3f3fb5ae7a28b63
Thiago da Silva Santos <thiagosfap@gmail.com>
2019-12-30 15:51:21 -0300

    Inclusao do Projeto

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        private static Usuario _usuarios = new Usuario();
+        public ActionResult Index()
+        {
+            return View( _usuarios.listaUsuario);
+        }
+
+        public ActionResult AdicionaUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdicionaUsuario(UsuarioModel _usuarioModel)
+        {
+            _usuarios.CriaUsuario(_usuarioModel);
+            return View();
+
+        }
+        public ViewResult DeletaUsuario(string id)
+        {
+            return View(_usuarios.GetUsuario(id));
+        }
+        [HttpPost]
+        public RedirectToRouteResult DeletaUsuario(string id, FormCollection collection)
+        {
+            _usuarios.DeletarUsuario(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Usuario
+    {
+        public List<UsuarioModel> listaUsuario = new List<UsuarioModel>();
+
+        public Usuario()
+        {
+            listaUsuario.Add(new UsuarioModel
+            {
+                nome = "Marcio",
+                sobrenome = "Braga",
+                endereco = "Rua piracanjuba, 100",
+                email = "marciobraga@gmail.com",
+                nascimento = Convert.ToDateTime("05/09/1975")
+
+            });
+
+            listaUsuario.Add(new UsuarioModel
+            {
+
+                nome = "Myke",
+                sobrenome = "Tyson",
+                endereco = "Rua da Tortura, 34",
+                email = "tyson@gmail.com",
+                nascimento = Convert.ToDateTime("13/08/1990")
+
+            });
+
+            listaUsuario.Add(new UsuarioModel
+            {
+
+                nome = "Debora Fonseca",
+                sobrenome = "Cabrini",
+                endereco = "Rua da Matriz, 103",
+                email = "debfon@gmail.com",
+                nascimento = Convert.ToDateTime("04/03/1988")
+                
+            });
+        }
+
+        public void CriaUsuario(UsuarioModel usuarioModelo)
+        {
+            listaUsuario.Add(usuarioModelo);
+        }
+
+        public void AtualizaUsuario(UsuarioModel usuarioModelo)
+        {
+            foreach (UsuarioModel usuario in listaUsuario)
+            {
+                if (usuario.email == usuarioModelo.email)
+                {
+                    listaUsuario.Remove(usuario);
+                    listaUsuario.Add(usuarioModelo);
+                    break;
+                }
+            }
+        }
+
+        public UsuarioModel GetUsuario(string Email)
+        {
+            UsuarioModel _usuarioModel = null;
+
+            foreach (UsuarioModel _usuario in listaUsuario)
+                if (_usuario.email == Email)
+                    _usuarioModel = _usuario;
+
+            return _usuarioModel;
+
+        }
+
+        public void DeletarUsuario(String Email)
+        {
+            foreach (UsuarioModel _usuario in listaUsuario)
+            {
+                if (_usuario.email == Email)
+                {
+                    listaUsuario.Remove(_usuario);
+
+                    break;
+                }
+            }
+        }
+    }
+
+        
+}
